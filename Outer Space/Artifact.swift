d7702e01ca5fd9d680fdc50f890479be91aa2640
Matheus Andrade <matheusvncs160@gmail.com>
2020-07-02 12:06:41 -0300

    Ajustes na mochila e novo joystick

--- Outer Space/Artifact.swift
+++ Outer Space/Artifact.swift
@@ -37,8 +37,8 @@ class ArtifactBank{
     
     init(){
         artifacts.append(Artifact(name: "Mochila", description: "A sua companheira mochila, sem ela você não conseguiria pular nessa atmosfera! Cuide muito bem dela.", imageName: "mochila"))
-        artifacts.append(Artifact(name: "Cogumelo Flambante", description: "Que raridade! Alguns povos de Kaateena matariam pelos efeitos desse cogumelo psicodélico. Pode usar, mas tenha cuidado para não perder a consiência do que é real!", imageName: "cogumelo"))
-        artifacts.append(Artifact(name: "Orbe Fajuto", description: "O seu primeiro orbe guiado, que te acompanha desde o começo. Mas ele é um pouco simples né? Talvez um dia você encontre um que te ajude de verdade.", imageName: "orbe"))
+        artifacts.append(Artifact(name: "Cogumelo Flambante", description: "Que raridade! Alguns povos de Kaateena matariam pelos efeitos desse cogumelo psicodélico. É comestível!", imageName: "cogumelo"))
+        artifacts.append(Artifact(name: "Orbe Singelo", description: "O seu primeiro orbe, que te acompanha desde o começo. Mas ele é um pouco simples, não é? Talvez...", imageName: "orbe"))
     }
     
     func foundArtifacts()->[Artifact]{
